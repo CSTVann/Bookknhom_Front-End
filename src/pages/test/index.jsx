30a90821921b24f496b8b42811595b94cff72e4b
Sotheavann Chhorn <sotheavannchhorn168@gmail.com>
2025-07-04 16:56:47 +0700

    s

--- src/pages/test/index.jsx
+++
@@ -1,7 +0,0 @@
-// Example in a React component or useEffect
-useEffect(() => {
-  fetch('http://localhost:8000/api/ping') // Use your backend URL
-    .then(res => res.json())
-    .then(data => console.log('Connected:', data))
-    .catch(err => console.error('Error:', err));
-}, []);
